c6b1f5ba13c3871273b6408ce611a697609d2fbd
readeral <alan@readeral.com>
2020-04-07 16:19:52 +1000

    spelling errors

--- src/components/pages/frontpage/RecommendedTrainingResources.js
+++ src/components/pages/frontpage/RecommendedTrainingResources.js
@@ -9,7 +9,7 @@ export default () => (
                 <div className="col-md-12">
                     <div className="section-title">
                         <h2 className="aquaHeader">Recommended Training Resources</h2>
-                        <p className="subtitle text-center">During the current COVID-19 shutdown, Ministry Training Tasmania wants to encourage Christian students and church members to consider investing in their own spiritual and theological development. We have prepared below a number of recommended resources, either to be studied alone or if there are others interested in the same material, you can join with a cohort facilitated by Ministry Training Tasmania. Please use the 'apply now' email address below to let us know if you're intenting to take one/some of these courses!</p>
+                        <p className="subtitle text-center">During the current COVID-19 shutdown, Ministry Training Tasmania wants to encourage Christian students and church members to consider investing in their own spiritual and theological development. We have prepared below a number of recommended resources, either to be studied alone or if there are others interested in the same material, you can join with a cohort facilitated by Ministry Training Tasmania. Please use the 'apply now' email address below to let us know if you're intending to take one/some of these courses!</p>
 
                         <div className="row margin-top-50">
                             <div className="col-md-3">
@@ -21,7 +21,7 @@ export default () => (
                                 </div>
                             </div>
                             <div className="col-md-9">
-                                <p className="ministryInfo">Ridley College in Melbourne is currently offering a free course 'Bible Overview', taught by Mike Raiter. This is an excellent unit for anyone interested in a better understanding of the Bible, and an excellent options for forming a cohort around.<br />You can find access to that course here: <a href="https://certificate.ridley.edu.au/courses/bible-overview">Bible overview</a>.</p>
+                                <p className="ministryInfo">Ridley College in Melbourne is currently offering a free course 'Bible Overview', taught by Mike Raiter. This is an excellent unit for anyone interested in a better understanding of the Bible, and is an excellent option for forming a cohort around.<br />You can find access to that course here: <a href="https://certificate.ridley.edu.au/courses/bible-overview">Bible overview</a>.</p>
                                 <p>For any students involved with Uni Fellowship (AFES Tasmania), there are additional courses available for a reduced fee, and a small number of scholarships available to assist with enrolment. Please use the info@ email contact below to enquire about these units.</p>
                             </div>
                         </div>
@@ -39,7 +39,7 @@ export default () => (
                                 </div>
                             </div>
                             <div className="col-md-9">
-                                <p className="ministryInfo">Moore College in Sydney have reduced the cost of their Preliminary Theological Certificate to $25 per unit for enrolments up until the 30th of May ($45 thereafter). Recommended units include 'Knowledge of God' (Doctrine 1) and 'Mark' (New Testament 1) and includes the opportunity to sit a formal online mutliple-choice exam. The PTC is great for preparing to step up at church, or as a stepping stone to formal Theological education.<br />You can enrol in a PTC unit here: <a href="https://moore.edu.au/courses/preliminary-theological-certificate/">Preliminary Theological Certificate</a></p>
+                                <p className="ministryInfo">Moore College in Sydney have reduced the cost of their Preliminary Theological Certificate to $25 per unit for enrolments up until the 30th of May ($45 thereafter). Recommended units include 'Knowledge of God' (Doctrine 1) and 'Mark' (New Testament 1) and includes the opportunity to sit a formal online multiple-choice exam. The PTC is great for preparing to step up at church, or as a stepping stone to formal Theological education.<br />You can enrol in a PTC unit here: <a href="https://moore.edu.au/courses/preliminary-theological-certificate/">Preliminary Theological Certificate</a></p>
                                 <p>For any students involved with Uni Fellowship (AFES Tasmania), there are a small number of scholarships available to assist with enrolment if you would like. Please use the info@ email contact below to enquire about the scholarships.</p>
                             </div>
                         </div>
@@ -54,7 +54,7 @@ export default () => (
                                 </div>
                             </div>
                             <div className="col-md-9">
-                                <p className="ministryInfo">Formal enrolment will not suit everybody's needs, however there are a number of excellent resources made available to online, a number of which we have hand-picked and recommend below:</p>
+                                <p className="ministryInfo">Formal enrolment will not suit everybody's needs, however there are a number of excellent resources made available online, a number of which we have hand-picked and recommend below:</p>
                                 <ul>
                                     <li><strong>Christ and Culture:</strong><br /><a href="https://www.abc.net.au/radionational/programs/boyerlectures/past-programs/?year=2005"><em>Peter Jensen: The Future of Jesus in Australia</em></a></li>
                                     <li><strong>Biblical Theology:</strong><br /><a href="https://certificate.ridley.edu.au/courses/bible-overview"><em>Ridley College: Bible Overview</em></a><br /><a href="https://moore.edu.au/courses/preliminary-theological-certificate/"><em>Moore College: Introduction to the Bible</em></a></li>
